e12c7f30bfd6553623a1fb441a09577f97019dcc
keefe <keefe@categorize.us>
2018-11-25 16:18:14 -0800

    verify json input/output on rest api

--- src/main/java/us/categorize/model/Message.java
+++ src/main/java/us/categorize/model/Message.java
@@ -1,5 +1,26 @@
 package us.categorize.model;
 
 public class Message {
-
+	private long internalId = -1;
+	private String body = null;
+	private String creator;
+	public long getInternalId() {
+		return internalId;
+	}
+	public void setInternalId(long internalId) {
+		this.internalId = internalId;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	} 
+	
 }
--- src/main/java/us/categorize/server/Messages.java
+++ src/main/java/us/categorize/server/Messages.java
@@ -1,16 +1,33 @@
 package us.categorize.server;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import us.categorize.model.Message;
 
 @Path("/messages")
 public class Messages {
 	@GET
 	@Path("test")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String test() {
-		return "Test";
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response test(@QueryParam("name") String name) {
+		Message message = new Message();
+		message.setCreator(name);
+		return Response.status(200).entity(message).build();
+	}
+	
+	@POST
+	@Path("consumeTest")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response consumeTest(Message message) {
+		message.setInternalId(42);
+		return Response.status(200).entity(message).build();
 	}
 }
