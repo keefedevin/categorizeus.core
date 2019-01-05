9cc991110cacb0d26eab89396cf91c6c6940fbf7
keefe <keefe@categorize.us>
2019-01-05 11:37:27 -0800

    model changes to support oauth login, remove stubs

--- src/main/java/us/categorize/api/UserStore.java
+++ src/main/java/us/categorize/api/UserStore.java
@@ -5,7 +5,9 @@ import us.categorize.model.User;
 public interface UserStore {
 	User getPrincipal(String sessionKey);
 	User getUser(String id);
+	User getUserByUserName(String userName);
 	boolean registerUser(User user);
+	boolean validateUser(User user);
 	boolean establishUserSession(User user, String sessionKey);
 	boolean destroySessionUser(String sessionUUID);
 }
